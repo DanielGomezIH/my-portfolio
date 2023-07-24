a9acf9156d681cc3e50cf1803c9f906fe33cc413
Daniel Gómez H <danielivgho5@gmail.com>
2023-07-24 15:07:39 -0500

    Index.html, styles.csss y main.js modificados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,25 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navclose = document.getElementById("nav-close");
 
+// MENU SHOW
+// Validate if constant exists
+
+if (navToggle) {
+  navMenu.toggleAttribute.addEventListener("Click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== ADD BLUR TO HEADER ===============*/
-
 
 /*=============== EMAIL JS ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
